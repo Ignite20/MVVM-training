358f43ba8f6f1c7e8e2b3a5919c18dfbca707021
a.madalin.gradinaru <a.madalin.gradinaru@accenture.com>
2018-07-11 13:10:05 +0200

    Fix keyboard not hiding when resuming the app and was shown before.

--- app/src/main/java/com/studio/ember/mvvm_training/screens/todo/ToDoActivity.java
+++ app/src/main/java/com/studio/ember/mvvm_training/screens/todo/ToDoActivity.java
@@ -3,9 +3,11 @@ package com.studio.ember.mvvm_training.screens.todo;
 import android.arch.lifecycle.Observer;
 import android.arch.lifecycle.ViewModelProviders;
 import android.content.Context;
+import android.content.res.Configuration;
 import android.graphics.Rect;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.v4.view.MotionEventCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.DividerItemDecoration;
 import android.support.v7.widget.LinearLayoutManager;
@@ -14,6 +16,7 @@ import android.support.v7.widget.helper.ItemTouchHelper;
 import android.util.Log;
 import android.view.MotionEvent;
 import android.view.View;
+import android.view.WindowManager;
 import android.view.inputmethod.InputMethodManager;
 import android.widget.EditText;
 import android.widget.ImageButton;
@@ -89,7 +92,8 @@ public class ToDoActivity extends AppCompatActivity implements TodoListAdapter.T
                 }
             }
         });
-
+        // Hide Keyboard when resuming
+        getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_STATE_HIDDEN);
     }
 
     @Override
@@ -107,26 +111,57 @@ public class ToDoActivity extends AppCompatActivity implements TodoListAdapter.T
 
     @Override
     protected void onStop() {
+        hideKeyboard(rv_todo_list);
         todoViewModel.update(adapter.getToDos());
         super.onStop();
     }
 
     @Override
+    protected void onResume() {
+        super.onResume();
+
+    }
+
+
+    @Override
     public boolean dispatchTouchEvent(MotionEvent event) {
         if (event.getAction() == MotionEvent.ACTION_DOWN) {
-            View v = getCurrentFocus();
-            if ( v instanceof EditText) {
-                if (!isPointInsideView(event.getRawX(), event.getRawY(), v)) {
-                    Log.i("", "!isPointInsideView");
-
-                    Log.i("", "dispatchTouchEvent clearFocus");
-                    v.clearFocus();
-                    InputMethodManager imm = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
-                    imm.hideSoftInputFromWindow(v.getWindowToken(), 0);
-                }
-            }
+            clearFocus(event);
+        }
+        else if(event.getAction() == MotionEvent.ACTION_SCROLL){
+            clearFocus(event);
         }
         return super.dispatchTouchEvent(event);
+    }
+
+    private void clearFocus(MotionEvent event) {
+        View v = getCurrentFocus();
+        if ( v instanceof EditText) {
+            if (!isPointInsideView(event.getRawX(), event.getRawY(), v)) {
+                Log.i("ET", "!isPointInsideView");
+
+                Log.i("ET", "dispatchTouchEvent clearFocus");
+                v.clearFocus();
+                hideKeyboard(v);
+            }
+        }else if(v instanceof RecyclerView){
+            if (isPointInsideView(event.getRawX(), event.getRawY(), v)) {
+                Log.i("RV", "!isPointInsideView");
+
+                Log.i("RV", "dispatchTouchEvent clearFocus");
+
+                hideKeyboard(v);
+            }
+        }
+
+    }
+
+    private void hideKeyboard(View v) {
+        if(v != null) {
+            InputMethodManager imm = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
+            imm.hideSoftInputFromWindow(v.getWindowToken(), InputMethodManager.RESULT_SHOWN);
+
+        }
     }
 
     private boolean isPointInsideView(float rawX, float rawY, View v) {
